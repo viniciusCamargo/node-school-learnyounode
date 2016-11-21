163c6eb93a438baf2c755dd197cb7572a2b6f42c
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-11-21 14:05:18 -0200

    Finish MY FIRST ASYNC I/O

--- ex04.js
+++ ex04.js
@@ -4,5 +4,6 @@ let file = process.argv[2]
 
 fs.readFile(file, 'utf-8', (err, content) => {
   if (err) {console.log(err)}
-  console.log(content)
+
+  console.log(content.split('\n').length -1)
 })
